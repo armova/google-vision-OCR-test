64d6203a1d3dc3390ea830cec71b630a55532161
Arnoldo Mora <a3@3vot.com>
2017-05-03 18:28:05 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+### google-vision-OCR-test
+#### Google vision OCR test tool
+
+Put your text images in the ./input directory and receive them converted to .txt files in the ./output directory.
+
+---
+#### Requirements
+Node.js installed in your computer/server
+
+Checkout how to set up your Google Cloud Vision account and project:
+[https://cloud.google.com/vision/docs/quickstart](https://cloud.google.com/vision/docs/quickstart)
+
+Will need a JSON Service Account key:
+[https://cloud.google.com/vision/docs/common/auth](https://cloud.google.com/vision/docs/common/auth)
+
+Overwrite projectId & keyFilename in index.js
+
+#### Run
+
+```
+npm install
+node index.js
+```
+---
---
+++ index.js
@@ -0,0 +1,39 @@
+const vision = require('@google-cloud/vision')({
+  // make sure to review https://cloud.google.com/vision/docs/quickstart
+  projectId: 'your-google-cloud-project-id-here',
+  keyFilename: 'your-google-cloud-json-key-file-name-here'
+});
+
+const fs = require ('fs');
+var recursive = require('recursive-readdir');
+
+let numFiles = 1;
+
+//search for all file in the input directory
+recursive('./input', ['input/.DS_Store'], function (err, files) {
+  files.forEach(function(file){
+    // The path to the local image file, e.g. "/path/to/image.png"
+    const fileName = './'+file;
+    transform(fileName);
+  })
+});
+
+// Read a local image and save content as a text document
+function transform(fileName){
+  vision.readDocument(fileName)
+  .then((results) => {
+    const fullTextAnnotation = results[1].responses[0].fullTextAnnotation;
+    const name = fileName+'.text';
+    // write the text file in the ooutput directory
+    fs.writeFile('./output/'+name , fullTextAnnotation.text, (err) => {
+      if (err) throw err;
+      console.log(numFiles + ' files have been saved!');
+      numFiles++;
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
+}
+
+
